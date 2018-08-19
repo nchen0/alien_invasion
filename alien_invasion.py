c34d8334037ad944ee43f82fc0d9691963f32b02
Nicky Chen <nicky.chen0@gmail.com>
2018-08-19 15:25:38 -0700

    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+import sys
+
+import pygame
+from settings import Settings
+
+
+def run_game():
+    # Initialize game and create a screen object.
+    pygame.init()
+
+    # Calling an instance of the Settings class
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Set the background color.
+    # bg_color = (230, 230, 230)
+
+    # Start the main loop for the game.
+    while True:
+
+        # Event listeners
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(ai_settings.bg_color)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+run_game()
